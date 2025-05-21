0630b8ebdb7ab9c5ffc94e2b315a8f7e31efffcb
LJ-desWeb <lujebor@gmail.com>
2025-05-22 01:44:29 +0200

    Add files via upload

---
+++ js/script_00.js
@@ -0,0 +1,17 @@
+
+// Scroll sueve -----  para ir del menu nav a cada seccion ------------------------------------
+document.querySelectorAll('a[href^="#"]').forEach(enlace => {
+  enlace.addEventListener('click', function(e) {
+    e.preventDefault();
+    const destino = document.querySelector(this.getAttribute('href'));
+    if (destino) {
+      destino.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
+
+
+
+
+
